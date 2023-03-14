dc4c40d9cfddd5b3ba6eb83c9f6d49b1c0900005
Augustine Ezinwa <augustineezinwa@gmail.com>
2023-03-14 03:50:43 +0100

    write from one file to another

--- index.mjs
+++ index.mjs
@@ -1,15 +1,28 @@
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
 
  
- const readFile = async () => {
-    try {
-    const fileContents =  await fs.readFile("./file.txt", "utf-8");
-    console.log(fileContents);
-    } catch(err) {
-        console.log(err);
-    }
- }
+//  const readFile = async () => {
+//     try {
+//     const fileContents =  await fs.readFile("./file.txt", "utf-8");
+//     console.log(fileContents);
+//     } catch(err) {
+//         console.log(err);
+//     }
+//  }
 
- readFile();
+//  readFile();
+
+// -------------------------------------------------------------------------
+
+import fs from 'fs';
+
+const readableStream = fs.createReadStream("./file.txt", { encoding: "utf-8"});
+const writableStream = fs.createWriteStream("./file2.txt");
+
+readableStream.on("data", (chunk) => {
+    console.log(chunk);
+    writableStream.write(chunk);
+});
+
   
    
